891f3f9f3616965c4412979cbe2f68f8e9d3d331
animallibraryn4 <nikhil7858978052@gmail.com>
2026-01-14 11:37:54 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -9,8 +9,6 @@ from pyrogram.enums import ParseMode
 from dotenv import load_dotenv
 from uvloop import install
 # Remove 'bot_loop' from this import line
-from bot import bot, Var, sch, LOGS, ffpids_cache, ff_queued
-import bot  # Import the module to assign global variables
 
 install()
 basicConfig(format="[%(asctime)s] [%(name)s | %(levelname)s] - %(message)s [%(filename)s:%(lineno)d]",
